b7f6503e7f9bb698d908f40ff2e5148d0582dd35
Wagner Leonardi <leonardiwagner@gmail.com>
2020-04-03 19:03:11 -0300

    add logger

--- nodejs-app/logger.js
+++ nodejs-app/logger.js
@@ -1,9 +1,20 @@
+const winston = require('winston')
+
+const logger = winston.createLogger({
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'webapp.log' })
+  ]
+});
+
 const info = (module, text) => {
-  return console.info(`[${module}] - ${text}`)
+  return logger.info(`[${module}] - ${text}`)
 }
 
 const error = (module, text) => {
-  return console.error(`[${module}] - ${text}`)
+  return logger.error(`[${module}] - ${text}`)
 }
 
 module.exports = { info, error }
